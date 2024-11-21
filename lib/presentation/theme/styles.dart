933fc227bcafd6e1702fbd7e0b4b771a67497a8a
HabibRouatbi <hebib@camelt-soft.com>
2024-11-21 20:23:38 +0100

    | 🦉 Feat  : styles

---
+++ lib/presentation/theme/styles.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+TextStyle robotoRegular = const TextStyle(
+  color: Colors.black,
+  fontFamily: "Roboto",
+  fontWeight: FontWeight.w400,
+);
+
+TextStyle robotoMedium = const TextStyle(
+  color: Colors.black,
+  fontFamily: "Roboto",
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle robotoBold = const TextStyle(
+  color: Colors.black,
+  fontFamily: "Roboto",
+  fontWeight: FontWeight.w700,
+);
+
+TextStyle robotoSemiBold = const TextStyle(
+  color: Colors.black,
+  fontFamily: "Roboto",
+  fontWeight: FontWeight.w600,
+);
