ab8de0bae777b8f3cf3a2d00d1db1d2c5662c617
HabibRouatbi <hebib@camelt-soft.com>
2024-11-21 20:09:14 +0100

    | 🦉 Feat  : colors

---
+++ lib/presentation/theme/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors { 
+  static const primary = Color(0xff5C59E3) ; 
+  static const accent = Color(0xff918FE1) ;
+  static const scaffold = Color(0xffE5E5E5) ;
+  
+}
