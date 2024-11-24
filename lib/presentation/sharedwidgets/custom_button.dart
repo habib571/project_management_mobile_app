c780d34a3ad5d242f23c1b1868681be07bc509f2
HabibRouatbi <hebib@camelt-soft.com>
2024-11-24 11:58:28 +0100

    ♻️ Refactor : main + button

--- lib/presentation/sharedwidgets/custom_button.dart
+++ lib/presentation/sharedwidgets/custom_button.dart
@@ -3,20 +3,21 @@ import 'package:flutter/material.dart';
 import '../utils/colors.dart';
 import '../utils/styles.dart';
 
-
 class CustomButton extends StatelessWidget {
-   CustomButton(
+  CustomButton(
       {super.key,
       this.buttonColor = AppColors.primary,
       required this.onPressed,
       required this.text,
-      this.widget = const SizedBox()});
+      this.widget = const SizedBox(),
+      this.height});
   final Color buttonColor;
   final String text;
   final Function() onPressed;
   final Widget widget;
-  final TextStyle btnTxtStyle =   robotoMedium.copyWith(color: Colors.white ,fontSize: 16) ;
-
+  final TextStyle btnTxtStyle =
+      robotoMedium.copyWith(color: Colors.white, fontSize: 16);
+  final double? height;
 
   @override
   Widget build(BuildContext context) {
@@ -25,22 +26,25 @@ class CustomButton extends StatelessWidget {
       children: [
         Expanded(
           flex: 2,
-          child: MaterialButton(
-            color: buttonColor,
-            padding: const EdgeInsets.symmetric(vertical: 12.0),
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
-            onPressed: onPressed,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                widget,
-                Text(
-                  text,
-                  style: btnTxtStyle,
-                  textAlign: TextAlign.center,
-                ),
-              ],
+          child: SizedBox(
+            height: height ?? 55,
+            child: MaterialButton(
+              color: buttonColor,
+              padding: const EdgeInsets.symmetric(vertical: 12.0),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40)),
+              onPressed: onPressed,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  widget,
+                  Text(
+                    text,
+                    style: btnTxtStyle,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
