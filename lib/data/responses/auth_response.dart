d965b68fbd2a7756a645dc76b6b137fcfbb8c01b
HabibRouatbi <hebib@camelt-soft.com>
2024-11-24 22:42:21 +0100

    🦉 Feat   : auth response

---
+++ lib/data/responses/auth_response.dart
@@ -0,0 +1,15 @@
+import 'package:project_management_app/domain/models/user.dart';
+
+class AuthResponse {
+  String token;
+  User user;
+
+  AuthResponse(this.token, this.user);
+  factory AuthResponse.fromJson(Map<String, dynamic> json) {
+    return AuthResponse(
+      json['token'] as String,
+      User.fromJson(json['user'] as Map<String, dynamic>),
+    );
+  }
+
+}
