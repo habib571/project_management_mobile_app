2761f981d8fe51556622d5aa0508be8fa54665b9
HabibRouatbi <hebib@camelt-soft.com>
2024-11-25 23:10:47 +0100

    🦉 Feat  : base view model

---
+++ lib/domain/usecases/signup_usecase.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:project_management_app/domain/repository/auth_repo.dart';
+
+import '../../data/network/failure.dart';
+import '../../data/network/requests.dart';
+import '../../data/responses/auth_response.dart';
+
+class SignupUseCase {
+  final AuthRepository _repository;
+  SignupUseCase(this._repository);
+  Future<Either<Failure, AuthResponse>> signup(RegisterRequest request) async =>
+      await _repository.signup(request);
+}
