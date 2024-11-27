fbeee9565c3354efa03ffdc9794acf8d8e97aef5
HabibRouatbi <hebib@camelt-soft.com>
2024-11-27 22:38:45 +0100

    🦉 Feat  : string extension

---
+++ lib/application/extensions/string_extension.dart
@@ -0,0 +1,29 @@
+extension StringExtensions on String? {
+  bool isNullOrEmpty() {
+    return this == null || this!.isEmpty;
+  }
+
+  String? isValidEmail() {
+    final expression = RegExp(
+      r"""^((?:[A-Za-z0-9!#$%&\'*+\-\/=?^_`{|}~]|(?<=^|\.)"|"(?=$|\.|@)|(?<=".*)[ .](?=.*")|(?<!\.)\.){1,64})(@)((?:[A-Za-z0-9.\-])*(?:[A-Za-z0-9])\.(?:[A-Za-z0-9]){2,})$""",
+      multiLine: true,
+    );
+
+    if (isNullOrEmpty() || !expression.hasMatch(this!)) {
+      return "Email address is not valid!";
+    }
+
+    return null;
+  }
+
+  String? isStrongPassword() {
+    final expression = RegExp(
+        r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$');
+
+    if (isNullOrEmpty() || !expression.hasMatch(this!)) {
+      return "Please enter a strong password";
+    }
+
+    return null;
+  }
+}
