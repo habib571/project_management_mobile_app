f7d928fbf7368e111d5361cdf1a1f402621a9b91
Rouatbi Ahmed <95349399+RouatbiAhmed@users.noreply.github.com>
2024-12-13 22:01:21 +0100

    ♻️ Refactor : Add expiresIn attribute

--- lib/data/responses/auth_response.dart
+++ lib/data/responses/auth_response.dart
@@ -2,11 +2,13 @@ import 'package:project_management_app/domain/models/user.dart';
 
 class AuthResponse {
   String token;
+  int expiresIn;
   User user;
-  AuthResponse(this.token, this.user);
+  AuthResponse(this.token, this.expiresIn ,this.user,);
   factory AuthResponse.fromJson(Map<String, dynamic> json) {
     return AuthResponse(
       json['token'] as String,
+      json['expiresIn'],
       User.fromJson(json['user'] as Map<String, dynamic>),
     );
   }
