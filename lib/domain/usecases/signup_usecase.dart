8193a33789df146069f305a99a237290f0539ee6
Rouatbi Ahmed <95349399+RouatbiAhmed@users.noreply.github.com>
2024-12-13 21:40:20 +0100

    ♻️ Refactor

--- lib/domain/usecases/signup_usecase.dart
+++ lib/domain/usecases/signup_usecase.dart
@@ -8,6 +8,5 @@ import '../../data/responses/auth_response.dart';
 class SignupUseCase {
   final AuthRepository _repository;
   SignupUseCase(this._repository);
-  Future<Either<Failure, AuthResponse>> signup(RegisterRequest request) async =>
-      await _repository.signup(request);
+  Future<Either<Failure, AuthResponse>> signup(RegisterRequest request) async => await _repository.signup(request);
 }
