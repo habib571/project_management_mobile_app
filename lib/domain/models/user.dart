d358a29e066ba463e972819ff4bfd46ff288dccc
Rouatbi Ahmed <95349399+RouatbiAhmed@users.noreply.github.com>
2024-12-13 21:35:57 +0100

    ♻️ Refactor : Add createdAt Attribute

--- lib/domain/models/user.dart
+++ lib/domain/models/user.dart
@@ -2,14 +2,16 @@ class User {
   int id;
   String fullName;
   String email;
+  DateTime createdAt;
 
-  User(this.id, this.fullName, this.email);
+  User(this.id, this.fullName, this.email, this.createdAt);
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
       json['id'] as int,
       json['fullName'] as String,
       json['email'] as String,
+      DateTime.parse(json['createdAt'])
     );
   }
   Map<String, dynamic> toJson() {
