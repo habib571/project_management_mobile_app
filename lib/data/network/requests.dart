a86b9b4da24874776dba9e91134ee8aba0f7e799
Rouatbi Ahmed <95349399+RouatbiAhmed@users.noreply.github.com>
2024-12-13 22:07:44 +0100

    ♻️ Refactor : Adapt th Register request class to the signin by making the fullName attribute optional

--- lib/data/network/requests.dart
+++ lib/data/network/requests.dart
@@ -1,19 +1,27 @@
 class RegisterRequest {
-  String fullName;
+  String? fullName;
   String email;
   String password;
 
   RegisterRequest({
     required this.email,
-    required this.fullName,
+    this.fullName,
     required this.password,
   });
 
   Map<String, dynamic> toJson() {
-    return {
-      'fullName': fullName,
+    if (fullName!= null && fullName!.isNotEmpty){
+      return {
+        'fullName': fullName,
+        'email': email,
+        'password': password,
+      };
+    }
+
+    return  {
       'email': email,
       'password': password,
     };
+
   }
 }
