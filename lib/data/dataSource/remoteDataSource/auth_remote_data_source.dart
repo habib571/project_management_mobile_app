05cb736890d2f9a7b3c0c43be60c545d83063967
asus <habib.rouatbi123@gmail.com>
2024-12-23 20:10:18 +0100

    ♻️ Refactor : added sigIn request

--- lib/data/dataSource/remoteDataSource/auth_remote_data_source.dart
+++ lib/data/dataSource/remoteDataSource/auth_remote_data_source.dart
@@ -1,11 +1,14 @@
 import 'package:http/http.dart';
 import 'package:project_management_app/application/functions/cruds_functions.dart';
-import 'package:project_management_app/data/network/requests.dart';
+import 'package:project_management_app/data/network/requests/auth_requests.dart';
 import 'package:project_management_app/data/responses/api_response.dart';
 import 'package:project_management_app/data/responses/auth_response.dart';
 
 abstract class AuthRemoteDataSource {
   Future<ApiResponse> signup(RegisterRequest request);
+  Future<ApiResponse> sigIn(SignInRequest request);
+
+
 }
 
 class AuthRemoteDataSourceImp implements AuthRemoteDataSource {
@@ -17,5 +20,16 @@ class AuthRemoteDataSourceImp implements AuthRemoteDataSource {
         onRequestResponse: (response, statusCode) {
           return ApiResponse(response, statusCode);
         });
+  }
+
+  @override
+  Future<ApiResponse> sigIn(SignInRequest request) async{
+    return await executePostRequest(
+        apiUrl: "/auth/signin",
+        body: request.toJson(),
+        onRequestResponse: (response, statusCode) {
+          return ApiResponse(response, statusCode);
+        });
+
   }
 }
