2653853b16cd1de1ae8aeecbec623326e1da4a62
asus <habib.rouatbi123@gmail.com>
2024-12-23 12:27:26 +0100

    ♻️ Refactor : added signin dependency_injection.dart

--- lib/application/dependencyInjection/dependency_injection.dart
+++ lib/application/dependencyInjection/dependency_injection.dart
@@ -1,4 +1,5 @@
 import 'package:get_it/get_it.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:project_management_app/data/repositoryImp/auth_repo_impl.dart';
 import 'package:project_management_app/domain/repository/auth_repo.dart';
@@ -6,6 +7,8 @@ import 'package:project_management_app/domain/usecases/signup_usecase.dart';
 
 import '../../data/dataSource/remoteDataSource/auth_remote_data_source.dart';
 import '../../data/network/internet_checker.dart';
+import '../../domain/usecases/signin_usecase.dart';
+import '../../presentation/modules/auth/viewmodel/signin-view_model.dart';
 import '../../presentation/modules/auth/viewmodel/signup_view_model.dart';
 
 GetIt instance = GetIt.instance;
@@ -17,7 +20,9 @@ initAppModule() {
   instance.registerLazySingleton<AuthRepository>(
       () => AuthRepositoryImpl(instance(), instance()));
   initSignupModule();
+  initSigninModule() ;
 }
+
 
 initSignupModule() {
   if (!GetIt.I.isRegistered<SignupUseCase>()) {
@@ -25,4 +30,16 @@ initSignupModule() {
     instance
         .registerFactory<SignupViewModel>(() => SignupViewModel(instance()));
   }
+}
+
+initSigninModule() {
+  if (!GetIt.I.isRegistered<SignInViewModel>()) {
+    instance.registerFactory<SignInUseCase>(()=>SignInUseCase(instance()));
+    instance.registerFactory<SignInViewModel>(() => SignInViewModel(instance()));
+  }
+}
+initGetStorageModule()  {
+  GetStorage.init();
+  instance.registerLazySingleton<GetStorage>(()=>GetStorage());
+
 }
