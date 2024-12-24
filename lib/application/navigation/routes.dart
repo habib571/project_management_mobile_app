497cf6ef37c197c6e092961f22a72255c0b8d198
asus <habib.rouatbi123@gmail.com>
2024-12-24 22:08:13 +0100

    ♻️ Refactor :  added middleware in routes

--- lib/application/navigation/routes.dart
+++ lib/application/navigation/routes.dart
@@ -1,9 +1,15 @@
 import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:project_management_app/application/dependencyInjection/dependency_injection.dart';
+import 'package:project_management_app/application/helpers/get_storage.dart';
+import 'package:project_management_app/application/navigation/middelware.dart';
 import 'package:project_management_app/application/navigation/routes_constants.dart';
 import 'package:project_management_app/presentation/modules/auth/view/screens/signin_screen.dart';
 import 'package:project_management_app/presentation/modules/auth/view/screens/signup_screen.dart';
+import 'package:project_management_app/presentation/modules/home/home_screen.dart';
 
-List<GetPage<dynamic>>? routes = [
-  GetPage(name: AppRoutes.login, page:()=> SigninScreen()) ,
+List<GetPage<dynamic>>? routes = [  GetPage(name: AppRoutes.login, page:()=> SigninScreen() ,middlewares:  [AuthMiddleware()]) ,
   GetPage(name: AppRoutes.signup, page:()=> const SignupScreen()) ,
+  GetPage(name: AppRoutes.signup, page:()=> const HomeNavBar()),
+
+
 ] ;
