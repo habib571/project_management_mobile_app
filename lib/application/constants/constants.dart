bd5615bb360882f50a6b20b3297071e889c427fa
asus <habib.rouatbi123@gmail.com>
2024-12-26 13:54:20 +0100

    ♻️ Refactor routes

--- lib/application/constants/constants.dart
+++ lib/application/constants/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
-  static const String baseUrl = "http://192.168.1.18:8000";
+  static const String baseUrl = "http://192.168.1.17:8000";
   static const String authToken = 'authToken';
   static const int expiresIn = 36000000;
   static const String createdAt = "";
