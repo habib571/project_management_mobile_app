a7e72549d49cb9159def676aa363b5e521da70e6
asus <habib.rouatbi123@gmail.com>
2024-12-26 11:44:04 +0100

    ♻️ Refactor home

--- lib/presentation/modules/home/home_screen.dart
+++ lib/presentation/modules/home/home_screen.dart
@@ -1,10 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
+
+import '../../utils/colors.dart';
+import '../auth/view/screens/signin_screen.dart';
+import '../auth/view/screens/signup_screen.dart';
 
 class HomeNavBar extends StatelessWidget {
   const HomeNavBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body :_showBody( context)
+    );
   }
-}
+
+
+  Widget _showBody(BuildContext context) {
+    return PersistentTabView(
+      context,
+      screens: _buildScreens(),
+      items: _navBarsItems(),
+      backgroundColor: AppColors.scaffold,
+      handleAndroidBackButtonPress: true,
+      resizeToAvoidBottomInset: true,
+      stateManagement: true,
+      navBarHeight: 60,
+      decoration: NavBarDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        colorBehindNavBar: AppColors.scaffold,
+        border: Border.all(color: AppColors.scaffold, width: 1.0),
+      ),
+      navBarStyle: NavBarStyle.style15,
+    );
+  }
+
+  List<Widget> _buildScreens() {
+    return [
+      const SignupScreen(),
+      const SignupScreen(),
+      SigninScreen(),
+    ];
+  }
+
+  List<PersistentBottomNavBarItem> _navBarsItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.home),
+        title: ("Home"),
+        activeColorPrimary: Colors.blue,
+        inactiveColorPrimary: AppColors.accent,
+        activeColorSecondary: AppColors.primary,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.add,),
+        title: ("Add"),
+        activeColorPrimary: AppColors.accent,
+        inactiveColorPrimary: AppColors.accent,
+        activeColorSecondary: AppColors.primary,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.person),
+        title: ("Profile"),
+        activeColorPrimary: Colors.blue,
+        inactiveColorPrimary: AppColors.accent,
+        activeColorSecondary: AppColors.primary,
+      ),
+    ];
+  }
+  }
+
+
+
