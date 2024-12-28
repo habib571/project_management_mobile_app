33cf727b377423ce7b730b6758dca4072552662b
Rouatbi Ahmed <95349399+RouatbiAhmed@users.noreply.github.com>
2024-12-29 00:34:20 +0100

    ♻️ Refactor : DI

--- lib/application/dependencyInjection/dependency_injection.dart
+++ lib/application/dependencyInjection/dependency_injection.dart
@@ -5,9 +5,11 @@ import 'package:project_management_app/application/helpers/get_storage.dart';
 import 'package:project_management_app/data/repositoryImp/auth_repo_impl.dart';
 import 'package:project_management_app/domain/repository/auth_repo.dart';
 import 'package:project_management_app/domain/usecases/signup_usecase.dart';
+import 'package:project_management_app/presentation/modules/addproject/add-project-view-model.dart';
 
 import '../../data/dataSource/remoteDataSource/auth_remote_data_source.dart';
 import '../../data/network/internet_checker.dart';
+import '../../presentation/modules/addproject/add-project_screen.dart';
 import '../helpers/token_mamanger.dart';
 import '../../domain/usecases/signin_usecase.dart';
 import '../../presentation/modules/auth/viewmodel/signin-view_model.dart';
@@ -33,6 +35,7 @@ initAppModule() async {
 
 initHomeModule() {
     instance.registerFactory<HomeViewModel>(() => HomeViewModel(instance()));
+    instance.registerFactory<AddProjectViewModel>(() => AddProjectViewModel(instance()));
 }
 
 initSignupModule() {
