0ce40304ef06630447fb9793a7999bf5578b4fc9
Rouatbi Ahmed <95349399+RouatbiAhmed@users.noreply.github.com>
2024-12-28 21:33:19 +0100

    ♻️ Refactor : general refactor

--- lib/application/constants/constants.dart
+++ lib/application/constants/constants.dart
@@ -1,6 +1,6 @@
 class Constants {
-  static const String baseUrl = "http://192.168.1.18:8000";
+  static const String baseUrl = "http://192.168.1.19:8000";
   static const String authToken = 'authToken';
   static const int expiresIn = 36000000;
-  static const String createdAt = "";
+  static const String createdAt = ""; // to delete
 }
