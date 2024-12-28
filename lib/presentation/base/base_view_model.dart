b5d0aaed36df8c1102622e84f0754b7cb560a893
Rouatbi Ahmed <95349399+RouatbiAhmed@users.noreply.github.com>
2024-12-28 15:21:21 +0100

    🦉 Feat : Add token monitoring function

--- lib/presentation/base/base_view_model.dart
+++ lib/presentation/base/base_view_model.dart
@@ -1,15 +1,27 @@
 import 'dart:async';
 import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:rxdart/rxdart.dart';
 
+import '../../application/helpers/get_storage.dart';
+import '../../application/navigation/routes_constants.dart';
+import '../../application/helpers/token_mamanger.dart';
+import '../stateRender/state_render.dart';
 import '../stateRender/state_render_impl.dart';
 
 abstract class BaseViewModel extends  ChangeNotifier implements BaseViewModelInputs ,
      BaseViewModelOutputs {
+
   late Stream<FlowState> _stateStream;
   final stateController = StreamController<FlowState>();
-  BaseViewModel() {
+  final TokenManager _tokenManager;
+
+  BaseViewModel(this._tokenManager,{bool startTokenMonitoringOnInit = true}) {
     _stateStream = stateController.stream.asBroadcastStream();
+    if (startTokenMonitoringOnInit) {
+      startTokenMonitoring();
+    }
   }
 
   @override
@@ -29,6 +41,16 @@ abstract class BaseViewModel extends  ChangeNotifier implements BaseViewModelInp
   @override
   void updateState(FlowState state) {
     stateController.add(state);
+  }
+
+  void startTokenMonitoring(){
+    _tokenManager.startTokenMonitoring();
+    _tokenManager.tokenValidityStream.listen((isTokenValid){
+      if(!isTokenValid){
+         updateState(ErrorState(StateRendererType.snackbarState, "Tek expired"),);
+        Get.offAllNamed(AppRoutes.login) ;
+      }
+    });
   }
 }
 
