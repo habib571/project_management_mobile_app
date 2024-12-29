33a5651d7608f177139f0b3e852b22949a47fe8c
Rouatbi Ahmed <95349399+RouatbiAhmed@users.noreply.github.com>
2024-12-29 16:57:10 +0100

    ♻️ Refactor : general refactor

--- lib/domain/repository/project_repo.dart
+++ lib/domain/repository/project_repo.dart
@@ -1,7 +1,8 @@
 import 'package:dartz/dartz.dart';
 import '../../data/network/failure.dart';
 import '../../data/responses/project_responce.dart';
+import '../models/project.dart';
 
 abstract class ProjectRepository {
-  Future<Either<Failure, ProjectResponce>> addProject(ProjectRepository projectRequest);
+  Future<Either<Failure, ProjectResponse>> addProject(Project projectRequest);
 }
