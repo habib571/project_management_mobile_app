ea75975774f7042acdcfeb3f99844298af9a7ec6
Rouatbi Ahmed <95349399+RouatbiAhmed@users.noreply.github.com>
2024-12-29 16:56:51 +0100

    🦉 Feat : Added ProjectResponse

--- lib/data/responses/project_responce.dart
+++ lib/data/responses/project_responce.dart
@@ -1,3 +1,13 @@
-class ProjectResponce{
+import '../../domain/models/project.dart';
 
+class ProjectResponse{
+  Project project ;
+
+  ProjectResponse(this.project);
+
+  factory ProjectResponse.fromJson(Map<String, dynamic> json) {
+    return ProjectResponse(
+      Project.fromJson(json['project'] as Map<String, dynamic>),
+    );
+  }
 }
