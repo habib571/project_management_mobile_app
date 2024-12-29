4c398668112878e269f8cc04e0f5c503482d15aa
Rouatbi Ahmed <95349399+RouatbiAhmed@users.noreply.github.com>
2024-12-29 16:57:28 +0100

    ♻️ Refactor : general refactor

--- lib/data/repositoryImp/project_repo_impl.dart
+++ lib/data/repositoryImp/project_repo_impl.dart
@@ -1,23 +1,42 @@
 
+import 'dart:developer';
+
 import 'package:dartz/dartz.dart';
 
 import 'package:project_management_app/data/network/failure.dart';
 
 import 'package:project_management_app/data/responses/project_responce.dart';
 
+import '../../domain/models/project.dart';
 import '../../domain/repository/project_repo.dart';
+import '../dataSource/remoteDataSource/project_data_source.dart';
+import '../network/error_handler.dart';
 import '../network/internet_checker.dart';
 
 class ProjectRepositoryImpl implements ProjectRepository{
   final NetworkInfo _networkInfo;
+  final ProjectDataSource _projectDataSource ;
 
-  ProjectRepositoryImpl(this._networkInfo)
+  ProjectRepositoryImpl(this._networkInfo,this._projectDataSource);
 
   @override
-  Future<Either<Failure, ProjectResponce>> addProject(ProjectRepository projectRequest) async {
-    if (await _networkInfo.isConnected) {}
-
-
+  Future<Either<Failure, ProjectResponse>> addProject (Project projectRequest) async {
+    if (await _networkInfo.isConnected) {
+      try {
+        final response = await _projectDataSource.addProject(projectRequest) ;
+        if (response.statusCode == 200) {
+          return Right(ProjectResponse.fromJson(response.data));
+        } else {
+          log(response.data) ;
+          return Left(Failure.fromJson(response.data));
+        }
+      }
+      catch (error) {
+        log(error.toString()) ;
+        return Left(ErrorHandler.handle(error).failure);
+      }
+    }
+    return Left(DataSource.NO_INTERNET_CONNECTION.getFailure());
   }
 
 }
