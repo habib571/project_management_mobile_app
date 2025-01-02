7527a877a3b4401b4f49ccf0cfb2d91417944cc0
asus <habib.rouatbi123@gmail.com>
2025-01-02 23:20:00 +0100

    🦉 Feat : added project details route

--- lib/application/navigation/routes.dart
+++ lib/application/navigation/routes.dart
@@ -5,6 +5,7 @@ import 'package:project_management_app/application/navigation/middelware.dart';
 import 'package:project_management_app/application/navigation/routes_constants.dart';
 import 'package:project_management_app/presentation/modules/auth/view/screens/signin_screen.dart';
 import 'package:project_management_app/presentation/modules/auth/view/screens/signup_screen.dart';
+import 'package:project_management_app/presentation/modules/dashboord/view/screens/projet_detail_screen.dart';
 import 'package:project_management_app/presentation/modules/home/home_screen.dart';
 
 import '../../presentation/modules/addproject/view/add-project_screen.dart';
@@ -13,5 +14,6 @@ List<GetPage<dynamic>>? routes = [  GetPage(name: AppRoutes.login, page:()=> Sig
   GetPage(name: AppRoutes.signup, page:()=> const SignupScreen()) ,
   GetPage(name: AppRoutes.home, page:()=>  HomeNavBar()),
   GetPage(name: AppRoutes.addproject, page:()=> AddProjectScreen()),
+  GetPage(name: AppRoutes.projectDetails, page:()=> const ProjectDetailScreen()),
 
 ] ;
