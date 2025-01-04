efb8691958643790a0c3f077b4238b16407d7361
asus <habib.rouatbi123@gmail.com>
2025-01-04 15:32:49 +0100

    ♻️ Refactor : project_card

--- lib/presentation/modules/dashboord/view/widgets/project_card.dart
+++ lib/presentation/modules/dashboord/view/widgets/project_card.dart
@@ -11,66 +11,62 @@ import 'package:project_management_app/presentation/utils/styles.dart';
 import '../../../../../domain/models/project.dart';
 
 class ProjectCard extends StatelessWidget {
-  const ProjectCard({super.key, required this.project});
+  const ProjectCard({super.key, required this.project, required this.onTap});
   final Project project ;
+  final Function onTap ;
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        Get.to(()=>const ProjectDetailScreen()) ;
-      },
-      child: Card(
-        color: AppColors.primary,
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Expanded(
-                flex: 3,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      project.name!,
-                      style: robotoMedium.copyWith(color: Colors.white, fontSize: 18),
-                    ),
-                    SizedBox(height: 22.h,) ,
-                    SizedBox(
-                      width: 110,
-                      child: CustomButton(
-                        padding: 5,
-                        onPressed: () {
-
-                        },
-                        text: 'View Project',
-                        height: 40,
-                        borderRadius: 12,
-                        textColor: AppColors.primary,
-                        buttonColor: Colors.white,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Expanded(
-                flex: 2,
-                child: CircularPercentIndicator(
-                  radius: 40,
-                  lineWidth: 10.0,
-                  percent: 0.8,
-                  center: Text(
-                    project.progress.toString(),
-                    style: robotoRegular.copyWith(color: Colors.white),
+    return Card(
+      color: AppColors.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              flex: 3,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    project.name!,
+                    style: robotoMedium.copyWith(color: Colors.white, fontSize: 18),
                   ),
-                  backgroundColor: AppColors.accent,
-                  progressColor: Colors.white
-                ),
+                  SizedBox(height: 22.h,) ,
+                  SizedBox(
+                    width: 110,
+                    child: CustomButton(
+                      padding: 5,
+                      onPressed: () {
+                        onTap() ;
+                      },
+                      text: 'View Project',
+                      height: 40,
+                      borderRadius: 12,
+                      textColor: AppColors.primary,
+                      buttonColor: Colors.white,
+                    ),
+                  )
+                ],
               ),
-            ],
-          ),
+            ),
+            Expanded(
+              flex: 2,
+              child: CircularPercentIndicator(
+                radius: 40,
+                lineWidth: 10.0,
+                percent: 0.8,
+                center: Text(
+                  project.progress.toString(),
+                  style: robotoRegular.copyWith(color: Colors.white),
+                ),
+                backgroundColor: AppColors.accent,
+                progressColor: Colors.white
+              ),
+            ),
+          ],
         ),
       ),
     );
