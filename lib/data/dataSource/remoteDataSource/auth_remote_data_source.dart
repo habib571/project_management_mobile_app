9ec464f13a942550d63138d9ef44655a440a223d
Rouatbi Ahmed <95349399+RouatbiAhmed@users.noreply.github.com>
2025-01-12 14:46:30 +0100

    🦉 Feat : Added getCurrentUserInfo

--- lib/data/dataSource/remoteDataSource/auth_remote_data_source.dart
+++ lib/data/dataSource/remoteDataSource/auth_remote_data_source.dart
@@ -4,12 +4,19 @@ import 'package:project_management_app/data/network/requests/auth_requests.dart'
 import 'package:project_management_app/data/responses/api_response.dart';
 import 'package:project_management_app/data/responses/auth_response.dart';
 
+import '../../../application/helpers/get_storage.dart';
+
 abstract class AuthRemoteDataSource {
   Future<ApiResponse> signup(RegisterRequest request);
   Future<ApiResponse> sigIn(SignInRequest request);
+  Future<ApiResponse> getCurrentUserInfo();
+
 }
 
 class AuthRemoteDataSourceImp implements AuthRemoteDataSource {
+  final LocalStorage _localStorage ;
+
+  AuthRemoteDataSourceImp(this._localStorage);
   @override
   Future<ApiResponse> signup(RegisterRequest request) async {
     return await executePostRequest(
@@ -27,6 +34,15 @@ class AuthRemoteDataSourceImp implements AuthRemoteDataSource {
         body: request.toJson(),
         onRequestResponse: (response, statusCode) {
           return ApiResponse(response, statusCode);
+        });
+  }
+  @override
+  Future<ApiResponse> getCurrentUserInfo()async{
+    return await executeGetRequest(
+        apiUrl: "/users/me",
+        bearerToken:  _localStorage.getAuthToken() ,
+        onRequestResponse: (response, statusCode) {
+          return ApiResponse(response , statusCode);
         });
   }
 }
