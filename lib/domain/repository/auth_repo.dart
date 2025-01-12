585de9cc8dcb099c51cd6d8e9858ce414c713143
Rouatbi Ahmed <95349399+RouatbiAhmed@users.noreply.github.com>
2025-01-12 14:46:11 +0100

    🦉 Feat : Added getCurrentUserInfo

--- lib/domain/repository/auth_repo.dart
+++ lib/domain/repository/auth_repo.dart
@@ -3,8 +3,11 @@ import 'package:dartz/dartz.dart';
 import '../../data/network/failure.dart';
 import '../../data/network/requests/auth_requests.dart';
 import '../../data/responses/auth_response.dart';
+import '../models/user.dart';
 
 abstract class AuthRepository {
   Future<Either<Failure, AuthResponse>> signup(RegisterRequest loginRequest);
   Future<Either<Failure, AuthResponse>> signIn(SignInRequest loginRequest);
+  Future<Either<Failure, User>> getCurrentUserInfo();
+
 }
