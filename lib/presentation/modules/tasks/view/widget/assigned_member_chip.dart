1f2ec69ea2153bc2941f8c2bc8aa78189a70689a
asus <habib.rouatbi123@gmail.com>
2025-01-16 22:57:12 +0100

    🦉 Feat : assigned user chip

---
+++ lib/presentation/modules/tasks/view/widget/assigned_member_chip.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:project_management_app/presentation/sharedwidgets/image_widget.dart';
+import 'package:project_management_app/presentation/utils/colors.dart';
+import 'package:project_management_app/presentation/utils/styles.dart';
+
+import '../../../../../domain/models/user.dart';
+
+class AssignedMemberChip extends StatelessWidget {
+  const AssignedMemberChip({super.key, required this.imageUrl, required this.userName, required this.onDeleted});
+ final String imageUrl ;
+ final String  userName ;
+ final Function onDeleted ;
+  @override
+  Widget build(BuildContext context) {
+    return InputChip(
+      label: Text(
+        userName ,
+        style: robotoBold.copyWith(fontSize: 12),
+      ),
+      backgroundColor: AppColors.accent,
+      avatar: ImagePlaceHolder(radius: 30, imageUrl: imageUrl) ,
+      shape:RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(18), // Adjust border radius here
+      ),
+      onDeleted: () => onDeleted(),
+    ) ;
+  }
+}
