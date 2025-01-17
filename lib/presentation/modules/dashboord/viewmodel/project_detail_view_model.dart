409d6372c055d60211a8afca0025c58a0af469cb
Rouatbi Ahmed <95349399+RouatbiAhmed@users.noreply.github.com>
2025-01-17 17:01:55 +0100

    ♻️ Refactor : general refactor

--- lib/presentation/modules/dashboord/viewmodel/project_detail_view_model.dart
+++ lib/presentation/modules/dashboord/viewmodel/project_detail_view_model.dart
@@ -1,4 +1,9 @@
+
+
+
+import 'package:flutter/material.dart';
 import 'package:project_management_app/domain/models/project_member.dart';
+import 'package:project_management_app/domain/models/user.dart';
 import 'package:project_management_app/domain/usecases/project/get_members.dart';
 import 'package:project_management_app/presentation/base/base_view_model.dart';
 import '../../../../application/dependencyInjection/dependency_injection.dart';
@@ -8,7 +13,8 @@ import '../../../stateRender/state_render_impl.dart';
 import 'dashboard_view_model.dart';
 
 class ProjectDetailViewModel extends BaseViewModel {
-  final GetMembersUseCase _useCase ;
+  final GetMembersUseCase _useCase;
+
   ProjectDetailViewModel(super.tokenManager, this._useCase);
 
   @override
@@ -17,23 +23,118 @@ class ProjectDetailViewModel extends BaseViewModel {
     getProjectMembers();
   }
 
-  final Project project= instance<DashBoardViewModel>().project ;
+  final Project project = instance<DashBoardViewModel>().project;
 
-  List<ProjectMember> _projectMember  = [];
-  List<ProjectMember> get projectMember => _projectMember ;
+  final List<ProjectMember> _projectMember = [];
+  List<ProjectMember> get projectMember => _projectMember;
+
+  final List<User> _memberToAdd = [];
+  List<User> get memberToAdd => _memberToAdd;
+
+  int _currentPage = 0;
+  int get currentPage => _currentPage;
+
+  bool _isLoadingMore = false;
+  bool get isLoadingMore => _isLoadingMore;
+
+  bool hasMore = true;
+
+  final int _pageSize = 11;
+
+  final ValueNotifier<FlowState> _stateNotifier = ValueNotifier(ContentState());
+  ValueNotifier<FlowState> get stateNotifier => _stateNotifier;
+
   getProjectMembers() async {
     updateState(LoadingState(
         stateRendererType: StateRendererType.fullScreenLoadingState));
 
     (await _useCase.getProjectMembers(project.id!)).fold(
-        (failure) {
-          updateState(ErrorState(StateRendererType.fullScreenErrorState, failure.message));
+            (failure) {
+          updateState(ErrorState(
+              StateRendererType.fullScreenErrorState, failure.message));
         },
-        (data) {
-          _projectMember = data ;
-          updateState(ContentState()) ;
+            (data) {
+          notifyListeners();
+          updateState(ContentState());
         }
-    ) ;
+    );
+  }
 
+
+  Future<void> addMember(String name, {int page = 0}) async {
+    if (_isLoadingMore || !hasMore) return;
+
+    if (page == 0) {
+      _memberToAdd.clear();
+      _currentPage = 0;
+      hasMore = true;
+      _stateNotifier.value = ContentState();
+    }
+
+    _isLoadingMore = true;
+
+    (await _useCase.getMemberByName(name, page, _pageSize)).fold(
+          (failure) {
+        _stateNotifier.value = ErrorState(StateRendererType.fullScreenErrorState, failure.message);
+      },
+          (data) {
+            final filteredData = data.where((user) =>user.fullName.toLowerCase()
+                .contains(name.toLowerCase())).toList();
+            if (filteredData.length < _pageSize) {
+              hasMore = false;
+              }
+
+        _memberToAdd.addAll(filteredData) ;
+        _currentPage = page + 1;
+        _stateNotifier.value = ContentState();
+      },
+    );
+
+    _isLoadingMore = false;
   }
 }
+
+
+
+
+
+
+
+
+
+
+/*
+
+  Future<void> addMember(String name, {int page = 0}) async {
+    if (_isLoadingMore || !hasMore) return;
+
+    if (page == 0) {
+      // New search, reset state.
+      _memberToAdd.clear();
+      _currentPage = 0;
+      hasMore = true;
+      _stateNotifier.value = ContentState();
+    }
+
+    _isLoadingMore = true;
+
+    (await _useCase.getMemberByName(name, page, _pageSize)).fold(
+          (failure) {
+        _stateNotifier.value = ErrorState(StateRendererType.fullScreenErrorState, failure.message);
+      },
+          (data) {
+            final filteredData = data.where((user) =>user.fullName.toLowerCase()
+                .contains(name.toLowerCase())).toList();
+            if (filteredData.length < _pageSize) {
+              hasMore = false; // No more data to fetch.
+              }
+
+        _memberToAdd.addAll(filteredData) ;
+        _currentPage = page + 1;
+        _stateNotifier.value = ContentState();
+      },
+    );
+
+    _isLoadingMore = false;
+  }
+ */
--- lib/presentation/sharedwidgets/custum_search_delegate.dart
+++ lib/presentation/sharedwidgets/custum_search_delegate.dart
@@ -1,31 +1,27 @@
+
+
 import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:project_management_app/application/extensions/screen_config_extension.dart';
 import 'package:project_management_app/presentation/modules/addmember/view/screens/add_member_screen.dart';
 import 'package:project_management_app/presentation/sharedwidgets/custum_listtile.dart';
+import 'package:provider/provider.dart';
 
 import '../../application/constants/constants.dart';
 import '../../domain/models/user.dart';
+import '../modules/dashboord/viewmodel/project_detail_view_model.dart';
+import '../stateRender/state_render_impl.dart';
 import 'image_widget.dart';
 
 
+class CustomSearchDelegate extends SearchDelegate {
+  final ProjectDetailViewModel viewModel;
 
-class CustomSearchDelegate extends SearchDelegate<User?> {
-
-  final List<User> names=[
-    User(1, "ahmed", "ahmed",Constants.userProfileImageUrl) , User(2, "rami", "rami",Constants.userProfileImageUrl) , User(3, "mohsssen", "mohsssen",Constants.userProfileImageUrl)
-  ] ;
-
-  final String searchLabel;
-
-  CustomSearchDelegate(this.searchLabel);
-
-  @override
-  String? get searchFieldLabel => searchLabel;
-
+  CustomSearchDelegate(this.viewModel);
 
   @override
   List<Widget>? buildActions(BuildContext context) {
@@ -34,6 +30,8 @@ class CustomSearchDelegate extends SearchDelegate<User?> {
         icon: const Icon(Icons.clear),
         onPressed: () {
           query = '';
+          viewModel.memberToAdd.clear();
+          showSuggestions(context);
         },
       ),
     ];
@@ -43,67 +41,203 @@ class CustomSearchDelegate extends SearchDelegate<User?> {
   Widget? buildLeading(BuildContext context) {
     return IconButton(
       icon: const Icon(Icons.arrow_back),
-      onPressed: () {
-        close(context, null);
-      },
+      onPressed: () => close(context, null),
     );
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    final List<User> results = names.asMap()
-        .entries
-        .where((entry) => entry.value.fullName.toLowerCase().contains(query.toLowerCase()))
-        .map((entry) => User(entry.key ,entry.value.fullName ,entry.value.email,entry.value.imageUrl))
-        .toList();
-    return AddMemberScreen(user: results[0]);
-      /*Padding(
-      padding:  EdgeInsets.symmetric(horizontal: 25.w),
-      child: ListView.builder(
-        itemCount: results.length,
-        itemBuilder: (context, index) {
-          return CustumListTitle(
-              leading:  ImagePlaceHolder(radius: 25.w,imageUrl: results[index].imageUrl) ,
-              subtitle: Text(results[index].email),
-              title: results[index].fullName,
-              onTap: () {
-                Get.to(AddMemberScreen(user: results[0]));
-                //close(context, results[index]);
-              });
-        },
-      ),
-    );*/
+    return _buildResultList();
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    final List<User> suggestions = names
-        .where((name) => name.fullName.toLowerCase().contains(query.toLowerCase()))
-        .toList();
+    if (query.isEmpty) {
+      return const Center(
+        child: Text("Type to search members..."),
+      );
+    }
 
-    return  Padding(
-      padding:  EdgeInsets.symmetric(horizontal: 25.w),
-      child: ListView.builder(
-        itemCount: suggestions.length,
-        padding: EdgeInsets.symmetric(vertical: 8.h),
-        itemBuilder: (context, index) {
-          return Padding(
-            padding:  EdgeInsets.symmetric(vertical: 8.h),
-            child: CustumListTitle(
-              leading: ImagePlaceHolder(radius: 25.w, imageUrl: suggestions[index].imageUrl),
-              subtitle: Text(suggestions[index].email),
-              title: suggestions[index].fullName,
-              onTap: () {
-                query = suggestions[index].fullName;
-                showResults(context);
+    // Call the addMember function each time the query changes
+    viewModel.addMember(query, page: 0);
+    viewModel.hasMore = true ;
+    return _buildResultList();
+  }
+
+  Widget _buildResultList() {
+    return ValueListenableBuilder<FlowState>(
+      valueListenable: viewModel.stateNotifier,
+      builder: (context, state, child) {
+        if (state is LoadingState && viewModel.memberToAdd.isEmpty) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (state is ErrorState) {
+          return Center(child: Text(state.message));
+        } else if (state is ContentState) {
+          final members = viewModel.memberToAdd;
+          if (members.isEmpty) {
+            return const Center(child: Text("No members found"));
+          }
+
+          return NotificationListener<ScrollNotification>(
+            onNotification: (scrollInfo) {
+              if (scrollInfo.metrics.pixels >= scrollInfo.metrics.maxScrollExtent &&
+                  !viewModel.isLoadingMore) {
+                // Fetch next page.
+                viewModel.addMember(query, page: viewModel.currentPage);
+              }
+              return false;
+            },
+            child: ListView.builder(
+              itemCount: members.length + 1,
+              itemBuilder: (context, index) {
+                if (index == members.length) {
+                  return viewModel.hasMore
+                      ? const Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Center(child: CircularProgressIndicator()),
+                  ): const SizedBox.shrink();
+                }
+
+                final member = members[index];
+                return CustumListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(member.imageUrl ?? ''),
+                  ),
+                  title: member.fullName,
+                  subtitle: member.email,
+                  onTap: () {
+                    Get.to(AddMemberScreen(user: member)) ;
+                    //close(context, member);
+                  },
+                );
               },
             ),
           );
-        },
-      ),
-    );
+        }
 
+        return const SizedBox.shrink();
+      },
+    );
   }
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+class CustomSearchDelegate extends SearchDelegate {
+  final ProjectDetailViewModel viewModel;
+
+  CustomSearchDelegate(this.viewModel);
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          query = ''; // Clear the search query.
+          viewModel.memberToAdd.clear(); // Clear current results.
+          showSuggestions(context); // Trigger search suggestion update.
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () => close(context, null),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return _buildResultList();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    if (query.isEmpty) {
+      return const Center(
+        child: Text("Type to search members..."),
+      );
+    }
+
+    // Call the addMember function each time the query changes
+    viewModel.addMember(query, page: 0); // Fetch first page of results.
+    viewModel.hasMore = true ;
+    return _buildResultList();
+  }
+
+  Widget _buildResultList() {
+    return ValueListenableBuilder<FlowState>(
+      valueListenable: viewModel.stateNotifier,
+      builder: (context, state, child) {
+        if (state is LoadingState && viewModel.memberToAdd.isEmpty) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (state is ErrorState) {
+          return Center(child: Text(state.message));
+        } else if (state is ContentState) {
+          final members = viewModel.memberToAdd;
+          if (members.isEmpty) {
+            return const Center(child: Text("No members found"));
+          }
+
+          return NotificationListener<ScrollNotification>(
+            onNotification: (scrollInfo) {
+              if (scrollInfo.metrics.pixels >= scrollInfo.metrics.maxScrollExtent &&
+                  !viewModel.isLoadingMore) {
+                // Fetch next page.
+                viewModel.addMember(query, page: viewModel.currentPage);
+              }
+              return false;
+            },
+            child: ListView.builder(
+              itemCount: members.length + 1,
+              itemBuilder: (context, index) {
+                if (index == members.length) {
+                  return viewModel.hasMore
+                      ? const Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Center(child: CircularProgressIndicator()),
+                  )
+                      : const SizedBox.shrink();
+                }
+
+                final member = members[index];
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(member.imageUrl ?? ''),
+                  ),
+                  title: Text(member.fullName),
+                  subtitle: Text(member.email),
+                  onTap: () {
+                    Get.to(AddMemberScreen(user: member)) ;
+                    //close(context, member);
+                  },
+                );
+              },
+            ),
+          );
+        }
+
+        return const SizedBox.shrink();
+      },
+    );
+  }
+}
+
+ */
