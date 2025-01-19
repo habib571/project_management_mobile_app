169c4a949cf10e0240ff3022b4941c74589ce8f4
asus <habib.rouatbi123@gmail.com>
2025-01-19 14:18:55 +0100

    🦉 Feat : added add task use case

---
+++ lib/domain/usecases/task/add_task_user_case.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:project_management_app/domain/models/task.dart';
+import 'package:project_management_app/domain/repository/task_repo.dart';
+
+import '../../../data/network/failure.dart';
+
+class AddTaskUseCase {
+  final TaskRepository _taskRepository ;
+  AddTaskUseCase(this._taskRepository);
+  Future<Either<Failure,TaskModel>> addTask(TaskModel request) async  => _taskRepository.addTask(request) ;
+
+}
