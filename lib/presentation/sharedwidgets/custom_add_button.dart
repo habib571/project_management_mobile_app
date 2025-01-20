a6da7c6ff3f611338d3ea848cd03721d363b3806
Rouatbi Ahmed <95349399+RouatbiAhmed@users.noreply.github.com>
2025-01-21 00:09:22 +0100

    🦉 Feat

---
+++ lib/presentation/sharedwidgets/custom_add_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:project_management_app/application/extensions/screen_config_extension.dart';
+
+import '../utils/colors.dart';
+
+class CustomAddButton extends StatelessWidget{
+  final Function()? onTap;
+  const CustomAddButton({super.key,required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 34.h,
+        width: 33.w,
+        decoration: BoxDecoration(
+          border: Border.all(color: AppColors.primary, width: 2),
+          shape: BoxShape.circle,
+        ),
+        child: const Center(
+          child: Icon(Icons.add,
+              size: 25, color: AppColors.primary), // Adjust size if needed
+        ),
+      ),
+    );
+  }
+
+}
