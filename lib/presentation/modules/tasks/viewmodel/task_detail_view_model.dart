17047793829ad464e1d75975822e94882b771681
asus <habib.rouatbi123@gmail.com>
2025-01-22 23:31:32 +0100

    🦉 Feat : added task detail view model w

---
+++ lib/presentation/modules/tasks/viewmodel/task_detail_view_model.dart
@@ -0,0 +1,35 @@
+import 'package:project_management_app/domain/models/task.dart';
+import 'package:project_management_app/presentation/base/base_view_model.dart';
+import 'package:project_management_app/presentation/modules/tasks/viewmodel/add_task_view_model.dart';
+import 'package:project_management_app/presentation/stateRender/state_render.dart';
+
+import '../../../stateRender/state_render_impl.dart';
+
+class TaskDetailsViewModel extends BaseViewModel {
+  TaskDetailsViewModel(super.tokenManager, this._addTaskViewModel);
+  final AddTaskViewModel _addTaskViewModel ;
+
+  TaskModel? _task ;
+  TaskModel get task => _task! ;
+  setTask(TaskModel task) {
+    _task = task ;
+  }
+  @override
+  void start() {
+    updateState(LoadingState(stateRendererType: StateRendererType.overlayLoadingState)) ;
+    getTaskDetail() ;
+    super.start();
+  }
+  void getTaskDetail() {
+    updateState(LoadingState(stateRendererType: StateRendererType.overlayLoadingState)) ;
+    setTask(_addTaskViewModel.task) ;
+
+    if(_task !=null) {
+       updateState(ContentState()) ;
+    }
+
+  }
+
+
+
+}
