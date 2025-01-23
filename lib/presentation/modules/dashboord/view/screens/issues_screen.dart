3c5a07755abd3e19ef0dad379217a0c3047bcf56
Rouatbi Ahmed <95349399+RouatbiAhmed@users.noreply.github.com>
2025-01-23 23:44:54 +0100

    ♻️ Refactor : general refactor

--- lib/presentation/modules/dashboord/view/screens/issues_screen.dart
+++ lib/presentation/modules/dashboord/view/screens/issues_screen.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:project_management_app/application/extensions/screen_config_extension.dart';
+import 'package:project_management_app/presentation/sharedwidgets/custom_appbar.dart';
 
+import '../../../../../application/helpers/get_storage.dart';
 import '../../../../../domain/models/user.dart';
 import '../../../../sharedwidgets/custom_issue_card.dart';
 import '../../../../utils/colors.dart';
 
 class IssuesScreen extends StatelessWidget{
-  const IssuesScreen({super.key});
+  const IssuesScreen(  {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +17,11 @@ class IssuesScreen extends StatelessWidget{
       backgroundColor: AppColors.scaffold ,
       body: ListView(
         children: [
+          SizedBox(height: 25.h,),
+          const CustomAppBar(title: "All Issues"),
+          SizedBox(height: 25.h,),
           IssueCard(
+            currentUserId: 100 ,
             title: 'API Bug',
             description: '500 Error',
             taskReference: 'XXX',
@@ -26,6 +33,17 @@ class IssuesScreen extends StatelessWidget{
             onMarkResolved: () {},
           ),
           IssueCard(
+            currentUserId: 100 ,
+            title: 'Error',
+            description: '500 Error',
+            taskReference: 'XXX',
+            taggedUsers: [],
+            createdBy:User(1, "Nabil", "nabil@gmail.com", 'https://images.unsplash.com/photo-1567784177951-6fa58317e16b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'),
+            isResolved: true,
+            onMarkResolved: () {},
+          ),
+          IssueCard(
+            currentUserId: 202 ,
             title: 'API Bug',
             description: '500 Error',
             taskReference: 'XXX',
@@ -38,10 +56,11 @@ class IssuesScreen extends StatelessWidget{
               User(1, "walid", "ahmed@gmail.com", 'https://images.unsplash.com/photo-1567784177951-6fa58317e16b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'),
 
             ],
-            createdBy:User(1, "Nabil", "nabil@gmail.com", 'https://images.unsplash.com/photo-1567784177951-6fa58317e16b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'),
+            createdBy:User(202, "Nabil", "nabil@gmail.com", 'https://images.unsplash.com/photo-1567784177951-6fa58317e16b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'),
             onMarkResolved: () {},
           ),
           IssueCard(
+            currentUserId: 202 ,
             title: 'API Bug',
             description: '500 Error',
             taskReference: 'XXX',
