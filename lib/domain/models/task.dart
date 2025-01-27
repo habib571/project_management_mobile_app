4c6d1c6c102ff6ca16aa0b0899830a523d808c1c
Rouatbi Ahmed <95349399+RouatbiAhmed@users.noreply.github.com>
2025-01-27 20:49:25 +0100

    🦉 Feat : Added TaskModel

---
+++ lib/domain/models/task.dart
@@ -0,0 +1,44 @@
+import 'package:project_management_app/domain/models/project.dart';
+import 'package:project_management_app/domain/models/user.dart';
+
+class TaskModel {
+  int? id;
+  String? name;
+  String? description;
+  String? deadline;
+  String? priority;
+  String? status ;
+  User? assignedUser;
+  int? assignedUserId;
+  Project? project ;
+
+
+  TaskModel(this.id, this.name, this.description, this.deadline, this.priority, this.assignedUser ,this.status,this.project);
+  TaskModel.request(this.name, this.description,this.deadline, this.priority ,this.assignedUserId, );
+  TaskModel.taggedTask(this.id, this.name);
+
+  factory TaskModel.fromJson(Map<String, dynamic> json) {
+    return TaskModel(
+      json['id'] ,
+      json['title'] ,
+      json['description'],
+      json['deadline'] ,
+      json['priority'] ,
+      json['assignedUser'] != null ? User.fromJson(json['assignedUser']) : null ,
+      json['status'],
+      json['project' ]!= null ? Project.fromJson(json['project']) : null ,
+
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'description': description,
+      'assignedTo': assignedUserId,
+      'deadline': deadline,
+      'priority': priority,
+    };
+  }
+}
+
