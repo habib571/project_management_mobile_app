10309b75a912492294c836c957cc9bd554df47c6
habib rouatbi <habi.rouatbi123@gmail.com>
2025-09-06 16:01:59 +0200

    🦉 Feat : voice_call_screen.dart

---
+++ lib/presentation/modules/dashboord/view/screens/project_details/voice_call_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:project_management_app/presentation/sharedwidgets/image_widget.dart';
+
+import '../../../../../utils/styles.dart';
+
+class VoiceCallScreen extends StatelessWidget {
+  const VoiceCallScreen(
+      {super.key, required this.name, required this.imageUrl});
+  final String name;
+  final String imageUrl;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.black12.withOpacity(0.7),
+        child: Center(
+            child: ImagePlaceHolder(
+                radius: 25, imageUrl: imageUrl, fullName: name)));
+  }
+}
