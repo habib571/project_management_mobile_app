0efa03e80d887adba8efef684417357e26a8d5e3
habib rouatbi <habi.rouatbi123@gmail.com>
2025-09-08 15:57:58 +0200

    🦉 Feat : add_meeting_request.dart

---
+++ lib/data/network/requests/add_meeting_request.dart
@@ -0,0 +1,26 @@
+import '../../../domain/models/meeting.dart';
+import '../../../domain/models/project_member.dart';
+
+class AddMeetingRequest {
+ final String title ;
+ final MeetingType? type ;
+ final int projectId ;
+ final String? startDateTime ;
+ final List<int> participantsIds ;
+
+  AddMeetingRequest( {
+    required this.title,
+    this.type,
+    required this.projectId,
+    this.startDateTime,
+    required this.participantsIds,
+  });
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'type': type?.toString().split('.').last,
+      'startDateTime': startDateTime,
+      'participantsIds': participantsIds,
+    };
+  }
+}
